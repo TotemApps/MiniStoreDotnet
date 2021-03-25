52307586cd6533f4f543d0259f36ab1f58ef80c5
Jairo Alberto Soto Velasquez <jasotov@eafit.edu.co>
2021-03-25 14:44:02 -0500

    Override methods lab

---
+++ src/SplLanguage/Dsl/CustomCode/FeatureElement.cs
@@ -0,0 +1,20 @@
+﻿using DslModeling = global::Microsoft.VisualStudio.Modeling;
+using DslDesign = global::Microsoft.VisualStudio.Modeling.Design;
+using System.Windows.Forms;
+
+/// <summary>
+/// Override methods in a partial class
+/// https://docs.microsoft.com/en-us/visualstudio/modeling/overriding-and-extending-the-generated-classes?view=vs-2019
+/// </summary>
+namespace Company.SplLanguage
+{
+    public partial class FeatureElement : DslModeling::ModelElement
+    {
+        protected override void OnDeleted()
+        {
+            MessageBox.Show("Deleting:" + this.GetDomainClass().GetType().Name + ", Name: " + this.Name);
+            base.OnDeleted();
+            
+        }
+    }
+}
